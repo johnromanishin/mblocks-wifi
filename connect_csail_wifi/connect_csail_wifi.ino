fbf17897401a9bbcb7ccac4d85284ac449c9bb7f
Sebastian Claici <sebastianclaici@gmail.com>
2016-03-31 16:02:38 -0400

    [ADD]: Post magnet reading to stream.

--- connect_csail_wifi/connect_csail_wifi.ino
+++ connect_csail_wifi/connect_csail_wifi.ino
@@ -2,9 +2,10 @@
   Connect to data.sparkfun.com and download the last command.
  */
 
-
 #include <SPI.h>
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Phant.h>
 
 char ssid[] = "StataCenter"; //  your network SSID (name)
 char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
@@ -14,8 +15,12 @@ int status = WL_IDLE_STATUS;
 
 // Phant server data
 char server[] = "data.sparkfun.com";    // name address for Google (using DNS)
-char PublicKey[] = "KJ7ZLKad2jIgDaOXNZpq";
-char PrivateKey[] = "vzrPJ8wdomT1vB742wlJ";
+
+char GetPublicKey[] = "KJ7ZLKad2jIgDaOXNZpq";
+char GetPrivateKey[] = "vzrPJ8wdomT1vB742wlJ";
+
+char PostPublicKey[] = "VG1a9d7p7gHzWLypO7E6";
+char PostPrivateKey[] = "9YkjplJ2JZfMJx7XpPjY";
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -27,9 +32,12 @@ const unsigned long postingInterval = 10L * 1000L;
 
 String responseLine;
 
+unsigned int magnet_reading;
+
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Wire.begin();
  
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -48,11 +56,25 @@ void setup() {
     // wait 10 seconds for connection:
     delay(1000);
   }
+  
   Serial.println("Connected to wifi");
   printWifiStatus();
 }
 
 void loop() {
+  // print out the sensor reading and post it online
+  Wire.beginTransmission(0x40);
+  Wire.write(byte(255));
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x40, 2);  // request 2 bytes from sensor
+  if (2 <= Wire.available()) {
+    magnet_reading = Wire.read();  // receive bigh byte  
+    magnet_reading <<= 6;
+    magnet_reading |= Wire.read();
+    Serial.println(magnet_reading);
+  }
+  
   // if there are incoming bytes available
   // from the server, read them and print them:
   while (client.available()) {
@@ -66,7 +88,8 @@ void loop() {
   }
 
   if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
+    postRequest();
+    getRequest();
   }
 }
 
@@ -76,7 +99,7 @@ bool cmdNext = false;
 void parseResponse(String line) {
   if (cmdNext) {
     int id = line.indexOf(',');
-    Serial.println("CMD: " + line.substring(0, id));
+    Serial.println(line.substring(0, id));
     Serial.println("TIME: " + line.substring(id + 1));
     lastTimestamp = line.substring(id + 1);
     cmdNext = false;  
@@ -86,14 +109,32 @@ void parseResponse(String line) {
   } 
 }
 
-void httpRequest() {
+void postRequest() {
+  WiFiClient postclient;
+  Phant phant(server, PostPublicKey, PostPrivateKey);
+
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.macAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String postedID = "ThingDev-" + macID;
+
+  phant.add("id", postedID);
+  phant.add("magnet", magnet_reading);
+  if (postclient.connect(server, 80)) {
+    postclient.print(phant.post());
+  }
+}
+
+void getRequest() {
   client.stop();
  
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
     // Make a HTTP request:
     client.print("GET /output/");
-    client.print(PublicKey);
+    client.print(GetPublicKey);
     client.print(".csv?limit=1");
     if (!lastTimestamp.equals("")) {
       client.print("&gt[timestamp]=");

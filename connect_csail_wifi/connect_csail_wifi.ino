ebba0cb3cfb22f204db22c43e3f724310c1c357d
Sebastian Claici <sebastianclaici@gmail.com>
2016-03-31 10:34:07 -0400

    [ADD]: Read and print last unseen stream command.
    Will re-print a command on reboot. This can be fixed by setting the
    timestamp to now. Would only do the last command seen after boot.

--- connect_csail_wifi/connect_csail_wifi.ino
+++ connect_csail_wifi/connect_csail_wifi.ino
@@ -1,138 +1,122 @@
-//I changed the digital to 8 and now can tell that it posted online!
+/*
+  Connect to data.sparkfun.com and download the last command.
+ */
 
-// Include the ESP8266 WiFi library. (Works a lot like the
-// Arduino WiFi library.)
+
+#include <SPI.h>
 #include <ESP8266WiFi.h>
-// Include the SparkFun Phant library.
-#include <Phant.h>
 
-//////////////////////
-// WiFi Definitions //
-//////////////////////
-const char WiFiSSID[] = "StataCenter";
-const char WiFiPSK[] = "";
+char ssid[] = "StataCenter"; //  your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
-/////////////////////
-// Pin Definitions //
-/////////////////////
-const int LED_PIN = 5; // Thing's onboard, green LED
-const int ANALOG_PIN = A0; // The only analog pin on the Thing
-const int DIGITAL_PIN = 12; // Digital pin to be read
+int status = WL_IDLE_STATUS;
 
-////////////////
-// Phant Keys //
-////////////////
-const char PhantHost[] = "data.sparkfun.com";
-const char PublicKey[] = "KJ7ZLKad2jIgDaOXNZpq";
-const char PrivateKey[] = "vzrPJ8wdomT1vB742wlJ";
+// Phant server data
+char server[] = "data.sparkfun.com";    // name address for Google (using DNS)
+char PublicKey[] = "KJ7ZLKad2jIgDaOXNZpq";
+char PrivateKey[] = "vzrPJ8wdomT1vB742wlJ";
 
-const int httpPort = 80;
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
 
-/////////////////
-// Post Timing //
-/////////////////
-const unsigned long postRate = 60000;
-unsigned long lastPost = 0;
+unsigned long lastConnectionTime = 0;
+const unsigned long postingInterval = 10L * 1000L;
 
-void setup() 
-{
-  initHardware(); // Setup input/output I/O pins
-  connectWiFi(); // Connect to WiFi
-  digitalWrite(LED_PIN, LOW); // LED on to indicate connect success
-}
+String responseLine;
 
-void loop() 
-{
-  // This conditional will execute every lastPost milliseconds
-  // (assuming the Phant post succeeded).
-  if ((lastPost + postRate <= millis()) || lastPost == 0)
-  {
-    Serial.println("Posting to Phant!");
-    if (getFromPhant())
-    {
-      lastPost = millis();
-    }
-    else // If the Phant post failed
-    {
-      delay(500); // Short delay, then try again
-      Serial.println("GET failed. Will try again.");
-    }
-  }
-}
-
-void connectWiFi()
-{
-  byte ledStatus = LOW;
-  Serial.println();
-  Serial.println("Connecting to: " + String(WiFiSSID));
-  // Set WiFi mode to station (as opposed to AP or AP_STA)
-  WiFi.mode(WIFI_STA);
-
-  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
-  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
-  // or WEP passphrase.
-  WiFi.begin(WiFiSSID, WiFiPSK);
-
-  // Use the WiFi.status() function to check if the ESP8266
-  // is connected to a WiFi network.
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    // Blink the LED
-    digitalWrite(LED_PIN, ledStatus); // Write LED high/low
-    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
-
-    // Delays allow the ESP8266 to perform critical tasks
-    // defined outside of the sketch. These tasks include
-    // setting up, and maintaining, a WiFi connection.
-    delay(100);
-    // Potentially infinite loops are generally dangerous.
-    // Add delays -- allowing the processor to perform other
-    // tasks -- wherever possible.
-  }
-  
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void initHardware()
-{
+void setup() {
+  //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  pinMode(DIGITAL_PIN, INPUT_PULLUP); // Setup an input to read
-  pinMode(LED_PIN, OUTPUT); // Set LED as output
-  digitalWrite(LED_PIN, HIGH); // LED off
-  // Don't need to set ANALOG_PIN as input, 
-  // that's all it can be.
+ 
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while(true);
+  }
+ 
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+ 
+    // wait 10 seconds for connection:
+    delay(1000);
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus();
 }
 
-int getFromPhant()
-{
-  // LED turns on when we enter, it'll go off when we 
-  // successfully post.
-  digitalWrite(LED_PIN, LOW);
-
-  WiFiClient client;
-  if (!client.connect(PhantHost, httpPort)) {
-      return 0;
-  }
-  
-  client.print("GET /output/");
-  client.print(PublicKey);
-  client.print(".csv");
-  client.print("?limit=1");
-  client.println(" HTTP/1.1");
-  client.print("Host: ");
-  client.println(PhantHost);
-  client.println("Connection: close");
-  client.println();
-
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
   while (client.available()) {
-      String line = client.readStringUntil('\r');
+    char c = client.read();
+    if (c == '\n') {
+      parseResponse(responseLine);
+      responseLine = "";
+    } else if (c != '\r') {
+      responseLine += c;
+    }
   }
-  
-  // Before we exit, turn the LED off.
-  digitalWrite(LED_PIN, HIGH);
 
-  return 1; // Return success
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
 }
 
+String lastTimestamp;
+bool cmdNext = false;
+
+void parseResponse(String line) {
+  if (cmdNext) {
+    int id = line.indexOf(',');
+    Serial.println("CMD: " + line.substring(0, id));
+    Serial.println("TIME: " + line.substring(id + 1));
+    lastTimestamp = line.substring(id + 1);
+    cmdNext = false;  
+  }
+  if (line.startsWith("cmd")) {
+    cmdNext = true;
+  } 
+}
+
+void httpRequest() {
+  client.stop();
+ 
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    // Make a HTTP request:
+    client.print("GET /output/");
+    client.print(PublicKey);
+    client.print(".csv?limit=1");
+    if (!lastTimestamp.equals("")) {
+      client.print("&gt[timestamp]=");
+      client.print(lastTimestamp);
+    }
+    client.println(" HTTP/1.1");
+    client.println("Host: data.sparkfun.com");
+    client.println("Connection: close");
+    client.println();
+
+    lastConnectionTime = millis();
+  } else {
+    Serial.println("connection failed");
+  }
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
